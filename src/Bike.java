120dfc7132e8b89c2242b8cb58542579b2357a29
lukasz <robson27-r@hotmail.com>
2019-02-10 16:33:02 +0100

    w trakcie

---
+++ src/Bike.java
@@ -0,0 +1,26 @@
+class Bike {
+
+    String brand;
+    int wheels;
+
+    public Bike(String brand, int wheels) {
+        this.brand = brand;
+        this.wheels = wheels;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        Bike[] bike1 = new Bike("Giant", 3);
+        Bike[] bike2 = new Bike ("Scott", 3);
+        Bike[] bike3 = new Bike ("Giant",2 );
+
+        int[]Bike  = new  int [3];
+        for (int i = 0; i < bike2.length ; i++) {
+
+        }
+
+
+        System.out.println(Bike.length);
+
+
+
+
+
+    }
+}
